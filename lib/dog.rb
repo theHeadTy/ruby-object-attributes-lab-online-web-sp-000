7911a5246f2d5719f22734fe36323242085c59f7
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-08 06:41:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+  attr_accessor :name, :breed
+
+end
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+puts snoopy.breed
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+  attr_accessor :name, :job
+
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+beyonce.job = 'Singer'
